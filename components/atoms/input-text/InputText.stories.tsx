a06de497a260b88f5c496f47605dd9efe283ecbf
hailey-hy <lillyine.hy@gmail.com>
2023-06-04 20:55:02 +0900

    style: storybook title 수정

--- components/atoms/input-text/InputText.stories.tsx
+++ components/atoms/input-text/InputText.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import InputText from './InputText';
 
 export default {
-    title: 'component: InputText',
+    title: 'InputText',
     component: InputText,
     tags: ['autodocs'],
 };
