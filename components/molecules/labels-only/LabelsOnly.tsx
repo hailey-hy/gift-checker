8b3b652a82d31b557a9f85649ac79031a67d4a37
hailey-hy <lillyine.hy@gmail.com>
2023-06-08 22:30:30 +0900

    feat: Labels-Only 기능 추가
    - paddingBottom prop으로 y축 padding 추가 가능
    - paddingBottom: 'true' | 'false';

--- components/molecules/labels-only/LabelsOnly.tsx
+++ components/molecules/labels-only/LabelsOnly.tsx
@@ -2,6 +2,7 @@ export type LabelsOnlyProps = {
     children: React.ReactNode;
     border: 'true' | 'false' | 'bottom';
     display: 'flex' | 'grid';
+    paddingBottom: 'true' | 'false';
 };
 
 const borders = {
@@ -12,17 +13,23 @@ const borders = {
 
 const displays = {
     flex: 'flex flex-row flex-wrap justify-center p-2 gap-10 sm:w-[550px] sm:p-4 ',
-    grid: 'grid gap-4 pb-2 sm:w-[630px] sm:grid-cols-custom sm:gap-8 ',
+    grid: 'grid gap-2 pb-2 p-2 text-center sm:w-[630px] sm:grid-cols-custom sm:gap-8 ',
 };
 
-const LabelsOnly = ({ children, border, display }: LabelsOnlyProps) => {
-    const style = displays[display] + borders[border];
+const paddingBottoms = {
+    true: 'pb-4 pt-4',
+    false: '',
+};
+
+const LabelsOnly = ({ children, border, display, paddingBottom }: LabelsOnlyProps) => {
+    const style = displays[display] + borders[border] + paddingBottoms[paddingBottom];
     return <div className={style}>{children}</div>;
 };
 
 LabelsOnly.defaultProps = {
     display: 'flex',
     border: 'true',
+    paddingBottom: 'false',
 };
 
 export default LabelsOnly;
