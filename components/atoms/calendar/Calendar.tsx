4c12bc458d1c282bbed8d4e08ad719d867ac5f75
hailey-hy <lillyine.hy@gmail.com>
2023-06-08 20:42:01 +0900

    style: 미사용 import 삭제

--- components/atoms/calendar/Calendar.tsx
+++ components/atoms/calendar/Calendar.tsx
@@ -1,9 +1,7 @@
-/** @jsx jsx */
 import * as React from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
-import { jsx, css } from '@emotion/react';
 
 const Calendar = () => {
     return (
