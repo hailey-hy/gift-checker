f8c872a45cddb27e9bb7477addd12a8d4276d864
hailey-hy <lillyine.hy@gmail.com>
2023-06-10 19:41:44 +0900

    feat: Home page 반응형 디자인 적용

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HomeTemplate from '@templates/home-template/HomeTemplate';
 
 const Home = () => {
     return (
-        <main className=' flex min-h-screen flex-col items-center justify-between p-0'>
+        <main className=' flex min-h-screen flex-col items-center justify-center xs:p-4'>
             <HomeTemplate></HomeTemplate>
         </main>
     );
