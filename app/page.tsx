4424c374eecc88920226b58847d917c5f7462322
hailey-hy <lillyine.hy@gmail.com>
2023-06-10 19:36:59 +0900

    feat: Home page 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import HomeTemplate from '@templates/home-template/HomeTemplate';
+
 const Home = () => {
-  return (
-    <main className=" flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
+    return (
+        <main className=' flex min-h-screen flex-col items-center justify-between p-0'>
+            <HomeTemplate></HomeTemplate>
+        </main>
+    );
 };
 
 export default Home;
